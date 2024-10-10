b91cb0eb660282f06a3278a987e327ef08926f7c
Marton Nagy <marton.nagy1@student.nhlstenden.com>
2024-10-10 16:22:58 +0200

    TestApp1

---
+++ TestApp/TestApp/AantalWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for AantalWindow.xaml
+    /// </summary>
+    public partial class AantalWindow : Window
+    {
+        public AantalWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Robot1_Click(object sender, RoutedEventArgs e)
+        {
+            CharacterWindow characterWindow = new CharacterWindow();
+            characterWindow.Show();
+            this.Close();
+        }
+
+        private void Robot2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
