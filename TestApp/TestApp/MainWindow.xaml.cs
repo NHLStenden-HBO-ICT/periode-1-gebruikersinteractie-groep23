c73a8b28c76468bdcf05023c89049d9948812dd7
Tjitte Brouwer <tjitte.brouwer@student.nhlstenden.com>
2024-10-11 17:54:57 +0200

    Optiescherm
    Optiescherm gemaakt met 2 sliders voor muziek en effecten, knoppen staan nog niet goed en geen comments

--- TestApp/TestApp/MainWindow.xaml.cs
+++ TestApp/TestApp/MainWindow.xaml.cs
@@ -16,12 +16,20 @@ namespace TestApp
         public MainWindow()
         {
             InitializeComponent();
+            MessageBox.Show(Convert.ToString(Globals.GlobalGeluid.effectenVolume));
         }
 
         private void Play_Click(object sender, RoutedEventArgs e)
         {
             AantalWindow aantalWindow = new AantalWindow();
             aantalWindow.Show();
+            this.Close();
+        }
+
+        private void btnOPtie_Click(object sender, RoutedEventArgs e)
+        {
+            OptieWindow optieWindow = new OptieWindow();
+            optieWindow.Show();
             this.Close();
         }
     }
