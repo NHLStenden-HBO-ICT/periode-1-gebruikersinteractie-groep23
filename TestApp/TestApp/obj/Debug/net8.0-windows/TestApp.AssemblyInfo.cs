3c3a4516d185e9c84966363a19de245e899cf6f0
Tjitte Brouwer <tjitte.brouwer@student.nhlstenden.com>
2024-10-16 11:09:06 +0200

    Heb meer globals toegevoegd en begonnen met een knop om het geluid helemaal uit te schakelen

--- TestApp/TestApp/obj/Debug/net8.0-windows/TestApp.AssemblyInfo.cs
+++ TestApp/TestApp/obj/Debug/net8.0-windows/TestApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d27a4bf8c3829cc5b85ec1b0dbead5dc165140")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
